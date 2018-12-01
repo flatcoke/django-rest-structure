424b4e6289292419a9976753e4b19c490aafd78a
FLATCOKE <flatcoke89@gmail.com>
2018-12-01 20:45:18 +0900

    valid env

--- flatcoke/wsgi.py
+++ flatcoke/wsgi.py
@@ -13,6 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)),
                                 '.env'))
+f = open('.env_example', 'r')
+for i in f.readlines():
+    if os.environ.get(i.rstrip()) is None:
+        raise EnvironmentError('must set env in list of env_example file')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flatcoke.settings")
 
--- manage.py
+++ manage.py
@@ -4,8 +4,29 @@ import sys
 import dotenv
 import os
 
+
+def valid_env():
+    env_list = []
+    env_example_list = []
+
+    f = open('.env', 'r')
+    for i in f.readlines():
+        env_list.append(i.rstrip().split('=')[0])
+
+    f = open('.env_example', 'r')
+    for i in f.readlines():
+        value = i.rstrip()
+        env_example_list.append(value)
+        if os.environ.get(value) is None:
+            raise EnvironmentError('must set env in list of env_example file')
+    if not set(env_example_list) == set(env_list):
+        raise EnvironmentError('env and env_example are not match')
+
+
 if __name__ == "__main__":
     dotenv.read_dotenv()
+    valid_env()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "flatcoke.settings.development")
     try:
